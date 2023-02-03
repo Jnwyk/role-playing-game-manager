4c0c4edc6caccc9303df1ba8e67a7830fef23134
Jan Nowak <nowak.jan@yahoo.com>
2023-02-03 12:07:20 +0100

    update init code

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -2,9 +2,17 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
+const App = () => (
   <React.StrictMode>
     <div>Hello</div>
   </React.StrictMode>
 );
+
+async function main() {
+  const root = document.createElement("div");
+  const body = document.querySelector("body");
+  body.appendChild(root);
+  ReactDOM.createRoot(root).render(<App />);
+}
+
+main();
