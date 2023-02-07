7ec60b931a75968c1ea748ce8bec3ac9c9d53b8b
Jan Nowak <nowak.jan@yahoo.com>
2023-02-07 11:33:41 +0100

    do validation of the register inputs

--- client/src/components/register-form/RegisterForm.jsx
+++ client/src/components/register-form/RegisterForm.jsx
@@ -2,8 +2,14 @@ import "./RegisterForm.css";
 import TextInput from "../text-input/TextInput";
 import Button from "../button/Button";
 import { useState } from "react";
+import validateRegister from "../../helpers/validateRegister";
 
 const RegisterForm = () => {
+  const [isFormRight, setIsFormRight] = useState({
+    username: true,
+    email: true,
+    password: true,
+  });
   const [userForm, setUserForm] = useState({
     username: "",
     email: "",
@@ -12,19 +18,47 @@ const RegisterForm = () => {
 
   const handleSetUserForm = (e) => {
     e.preventDefault();
-    setUserForm({
-      username: e.target[0].value,
-      email: e.target[1].value,
-      password: e.target[2].value,
-    });
+    if (
+      validateRegister(e.target[0].value, e.target[1].value, e.target[2].value)
+    ) {
+      setIsFormRight({
+        username: true,
+        email: true,
+        password: true,
+      });
+      setUserForm({
+        username: e.target[0].value,
+        email: e.target[1].value,
+        password: e.target[2].value,
+      });
+    } else {
+      setIsFormRight(false);
+      setUserForm({
+        username: "",
+        email: "",
+        password: "",
+      });
+    }
   };
 
   return (
     <div className="register-form__container">
       <form onSubmit={handleSetUserForm} className="register-form__form">
-        <TextInput placeholder="Username" id="username" />
-        <TextInput placeholder="Email" id="email" />
-        <TextInput placeholder="Password" id="password" />
+        <TextInput
+          placeholder="Username"
+          id="username"
+          isRight={isFormRight ? "right" : "wrong"}
+        />
+        <TextInput
+          placeholder="Email"
+          id="email"
+          isRight={isFormRight ? "right" : "wrong"}
+        />
+        <TextInput
+          placeholder="Password"
+          id="password"
+          isRight={isFormRight ? "right" : "wrong"}
+        />
         <Button type="submit ">Register</Button>
       </form>
     </div>
--- client/src/components/text-input/TextInput.jsx
+++ client/src/components/text-input/TextInput.jsx
@@ -1,9 +1,9 @@
 import "./TextInput.css";
 
-const TextInput = ({ placeholder, id }) => {
+const TextInput = ({ placeholder, id, isRight }) => {
   return (
     <input
-      className="text-input"
+      className={`text-input${isRight === "wrong" ? " wrong" : ""}`}
       type="text"
       id={id}
       name={id}
