5b1e9dcb4f4ece4163183a7d58bfbcd618f5cadb
Jan Nowak <nowak.jan@yahoo.com>
2023-02-07 22:27:23 +0100

    change button style

--- client/src/components/button/Button.jsx
+++ client/src/components/button/Button.jsx
@@ -2,7 +2,7 @@ import "./Button.css";
 
 const Button = ({ children, type, onClick, className }) => {
   return (
-    <button type={type} className={`button ${className}`} onClick={onClick}>
+    <button type={type} className={`${className} button`} onClick={onClick}>
       {children}
     </button>
   );
