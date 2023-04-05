347989ceb94c96bb44f65fee4debd8d52acac1ef
Jan Nowak <nowak.jan@yahoo.com>
2023-04-05 21:04:36 +0200

    add /games endpoint for test

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,5 +1,9 @@
 const { Router } = require("express");
 const googleAuth = require("./google-auth.js");
 const test = require("./test.js");
+const games = require("./games.js");
 
-module.exports = Router().get("/test", test).use("/auth", googleAuth);
+module.exports = Router()
+  .get("/test", test)
+  .use("/auth", googleAuth)
+  .use("/games", games);
