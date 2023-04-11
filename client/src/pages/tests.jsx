b734a3616bc9ddfa5b2b3997727fa63388987fdf
Jan Nowak <nowak.jan@yahoo.com>
2023-04-11 14:15:37 +0200

    add UserCard component

--- client/src/pages/tests.jsx
+++ client/src/pages/tests.jsx
@@ -3,6 +3,7 @@ import RegisterForm from "../components/register-form/RegisterForm";
 import LoginForm from "../components/login-form/LoginForm";
 import TextInput from "../components/text-input/TextInput";
 import LoginFooter from "../components/login-footer/LoginFooter";
+import UserCard from "../components/user-card/UserCard";
 
 const Tests = () => {
   return (
@@ -15,6 +16,12 @@ const Tests = () => {
       <br />
       <h2>&lt;Login Form /&gt;</h2>
       <LoginForm />
+      <br />
+      <h2>&lt;UserCard /&gt;</h2>
+      <UserCard
+        username="Jnwyk"
+        photo="https://upload.wikimedia.org/wikipedia/commons/thumb/7/71/Calico_tabby_cat_-_Savannah.jpg/1200px-Calico_tabby_cat_-_Savannah.jpg"
+      />
       <br />
       <div>
         <h2>&lt;Login Footer /&gt;</h2>
