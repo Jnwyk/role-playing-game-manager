5832708855fdbc61914be5a28f86dbe6dac4add7
Jan Nowak <nowak.jan@yahoo.com>
2023-04-14 10:49:59 +0200

    add GameCard component

---
+++ client/src/components/game-card/GameCard.jsx
@@ -0,0 +1,28 @@
+import "./GameCard.css";
+
+const GameCard = ({ game }) => {
+  return (
+    <div className="game-card">
+      <img
+        src="https://www.cdprojekt.com/en/wp-content/uploads-en/2022/03/16-9-en-1024x576.png"
+        alt="Game picture"
+        className="game-card__picture"
+      />
+      <div className="game-card__info">
+        <h2 className="game-card__title">Title of the game</h2>
+        <div></div>
+        <p className="game-card__description">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat. Duis aute irure dolor in
+          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+          culpa qui officia deserunt mollit anim id est laborum.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default GameCard;
