539116327e113b6a53bee193e35ac76ebedecff9
Jan Nowak <nowak.jan@yahoo.com>
2023-06-05 15:32:20 +0200

    add refreshing spotify token

--- server/routes/spotify.js
+++ server/routes/spotify.js
@@ -1,9 +1,11 @@
 const { Router } = require("express");
+const spotifyWebApi = require("spotify-web-api-node");
 const querystring = require("querystring");
+const SpotifyWebApi = require("spotify-web-api-node");
 
 const login = (req, res) => {
   const clientId = "58bb2cc230f84df699fcd6874f054666";
-  const redirectUri = "http://localhost:3000/games";
+  const redirectUri = "http://localhost:3000/music";
   const scope =
     "streaming user-read-email user-read-private user-library-read user-library-modify user-read-playback-state user-modify-playback-state user-read-currently-playing";
   const url =
@@ -14,8 +16,49 @@ const login = (req, res) => {
       scope: scope,
       redirect_uri: redirectUri,
     });
-  console.log(url);
   return res.redirect(302, url);
 };
 
-module.exports = Router().get("/login", login);
+const access = (req, res) => {
+  const code = req.body.code;
+  const spotifyApi = new SpotifyWebApi({
+    redirectUri: "http://localhost:3000/music",
+    clientId: "58bb2cc230f84df699fcd6874f054666",
+    clientSecret: "de3a0df57fa241f2aada7170975ff6a0",
+  });
+
+  spotifyApi
+    .authorizationCodeGrant(code)
+    .then((data) =>
+      res.json({
+        accessToken: data.body.access_token,
+        refreshToken: data.body.refresh_token,
+        expiresIn: data.body.expires_in,
+      })
+    )
+    .catch((err) => res.status(400).json({ err: err }));
+};
+
+const refresh = (req, res) => {
+  const refreshToken = req.body.refreshToken;
+  const spotifyApi = new SpotifyWebApi({
+    redirectUri: "http://localhost:3000/music",
+    clientId: "58bb2cc230f84df699fcd6874f054666",
+    clientSecret: "de3a0df57fa241f2aada7170975ff6a0",
+    refreshToken,
+  });
+  spotifyApi
+    .refreshAccessToken()
+    .then((data) => {
+      res.json({
+        accessToken: data.body.access_token,
+        expiresIn: data.body.expires_in,
+      });
+    })
+    .catch((err) => res.status(400));
+};
+
+module.exports = Router()
+  .get("/login", login)
+  .post("/login/access", access)
+  .post("/login/refresh", refresh);
