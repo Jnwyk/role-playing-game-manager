1343b6f7eaaa24e0882f1cf3a1b5db6ad63e7200
Jan Nowak <nowak.jan@yahoo.com>
2023-07-03 11:33:49 +0200

    add styling

--- client/src/components/profile-card/ProfileCard.jsx
+++ client/src/components/profile-card/ProfileCard.jsx
@@ -3,7 +3,7 @@ import Card from "../UI/card/Card";
 import Button from "../UI/button/Button";
 import axios from "axios";
 
-const ProfileCard = () => {
+const ProfileCard = ({ user }) => {
   const handleLogOut = async () => {
     await axios.get("/api/auth/logout").then((res) => {
       if (res.data.message === "logout") {
@@ -19,18 +19,18 @@ const ProfileCard = () => {
         src="https://polki.pl/foto/4_3_LARGE/ile-kosztuje-fretka-domowa-co-je-i-jak-o-nia-dbac-2487860.jpg"
         alt="user-profile"
       />
-      <div className="profile-card__data">
+      <div className="profile-card__data-container">
         <div className="profile-card__data">
           <h6 className="profile-card__title">username</h6>
-          <p className="profile-card__text">Jnwyk</p>
+          <p className="profile-card__text">{user.username}</p>
         </div>
         <div className="profile-card__data">
           <h6 className="profile-card__title">email</h6>
-          <p className="profile-card__text">jasio1908@gmail.com</p>
+          <p className="profile-card__text">{user.email}</p>
         </div>
         <div className="profile-card__data">
           <h6 className="profile-card__title">password</h6>
-          <p className="profile-card__text">*******</p>
+          <p className="profile-card__text">********</p>
         </div>
       </div>
       <Button onClick={() => handleLogOut()}>LOG OUT</Button>
--- client/src/pages/profile.jsx
+++ client/src/pages/profile.jsx
@@ -1,11 +1,15 @@
 import "./styles.css";
+import { useContext } from "react";
+import { LoggedUserContext } from "..";
 import Page from "../components/page/Page";
 import ProfileCard from "../components/profile-card/ProfileCard";
 
 const Profile = () => {
+  const userInfo = useContext(LoggedUserContext);
+
   return (
     <Page>
-      <ProfileCard />
+      <ProfileCard user={userInfo.user} />
     </Page>
   );
 };
